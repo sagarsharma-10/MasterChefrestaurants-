e16a3e631bbc533bd4cc1a7018aa5ae0002b67c2
Sagar Sharma <jackparker0121@gmail.com>
2026-04-01 11:43:21 +0530

    [Main change]

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import UseContext from './UseContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter >
+  <HashRouter >
     <UseContext >
       <App />
     </UseContext>
-  </BrowserRouter>
+  </HashRouter>
 )
